d999dec9c9cd72a36b12dbf60a4595fe6aab73dd
IgnacyMigas <ignacy@debian>
2017-03-27 16:29:28 +0200

    Solve problem with test

--- parser/parser_test.py
+++ parser/parser_test.py
@@ -8,13 +8,15 @@ from exception import *
 class LogParser_test(unittest.TestCase):
 	def test_with_no_file(self):
 		filename = "test.dat"
-		wc = WordCounter(filename)
-		self.assertRaises(NotSuchFile, wc.countWord)
+		string = "PrChecker.Downs"
+		parser = LogParser(string, filename)
+		self.assertRaises(NotSuchFile, parser.parse)
 	
 	def test_division_by_zero(self):
 		filename = 1.3
-		wc = WordCounter(filename)
-		self.assertRaises(NotAString, wc.countWord)
+		string = "PrChecker.Downs"
+		parser = LogParser(string, filename)
+		self.assertRaises(NotAString, parser.parse)
 
 if __name__ == "__main__":
 	unittest.main()
